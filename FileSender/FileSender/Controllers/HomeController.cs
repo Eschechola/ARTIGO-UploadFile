fe2e5a574a6aa89d5a89c019f1add988a80ec9f5
Eschechola <lucas.eschechola@outlook.com>
2020-05-31 05:02:57 -0300

    Adicionando projeto

---
+++ FileSender/FileSender/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using FileSender.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace FileSender.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        public IActionResult Index()
+        {
+            ViewBag.UrlImage = "/images/default.png";
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(IFormFile Upload)
+        {
+            try
+            {
+                //verifica se algum arquivo foi enviado
+                if (Upload is null || Upload?.Length == 0)
+                    return Redirect("/");
+                
+
+                //pega o diretório de upload que está no arquivo de configuração
+                var directory = _configuration["Directories:Uploads"];
+
+                //salva o arquivo no diretório informado
+                var filePath = new FileManager().SaveFile(Upload, directory);
+
+                //atribui a URL da imagem para a viewBag removendo a "wwwroot"
+                ViewBag.UrlImage = filePath.Remove(0, 7);
+
+                return View();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ FileSender/FileSender/Utilities/FileManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+
+namespace FileSender.Utilities
+{
+    public class FileManager
+    {
+        public string SaveFile(IFormFile Upload, string uploadDirectory)
+        {
+            //atribui um novo nome aleatório ao arquivo
+            var newFileName = Guid.NewGuid().ToString().Substring(0, 10);
+
+            //pega a extensão do arquivo a partir da última ocorrência do .
+            //subtraindo com o tamanho da string
+            int startIndex = Upload.FileName.LastIndexOf('.');
+            int endIndex = Upload.FileName.Length;
+            int lenght = endIndex - startIndex;
+
+            var fileType = Upload.FileName.Substring(startIndex, lenght);
+
+            //verifica se existe algum arquivo com o mesmo nome
+            //caso haja gera um novo nome
+            while (File.Exists(Path.Combine(uploadDirectory, newFileName + fileType)))
+            {
+                newFileName = Guid.NewGuid().ToString().Substring(0, 10);
+            }
+
+            //salva a imagem no diretório informado
+            //através de stream de dados
+            var filePath = Path.Combine(uploadDirectory, newFileName + fileType);
+
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                Upload.CopyTo(fileStream);
+            }
+
+            return filePath;
+        }
+    }
+}
